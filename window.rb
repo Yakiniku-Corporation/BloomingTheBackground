93f98cd3116689c6422c05537608bc2ada12a6cd
Mamoliena <54253130+Mamoliena@users.noreply.github.com>
2020-02-25 21:02:12 +0900

    Add files via upload

--- window.rb
+++ window.rb
@@ -1,10 +1,10 @@
 require'dxruby'
 require'./option.rb'
 #coding:shift_jis
-Window.caption     = "Blooming the background"
-Window.width       = @window_width  = 1920
-Window.height      = @window_height = 1080
-Window.full_screen = true
+#Window.caption     = "Blooming the background"
+#Window.width       = @window_width  = 1920
+#Window.height      = @window_height = 1080
+#Window.full_screen = true
 
 @option_draw = Option.new()
 @white       = Image.new(1920,1080,[255,255,255])
