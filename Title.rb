0e47c26482f7d7e6d61a5195265d7944606af200
nisini <61419352+nisini@users.noreply.github.com>
2020-02-27 22:51:50 +0900

    タイトル画面に必要なパーツを集めたclassです
    単体で使えます

--- Title.rb
+++ Title.rb
@@ -1,32 +1,76 @@
 require 'dxruby'
-require './Fps'
+
 class Title
 
   def initialize()
     Window.width=1920
-    @@width=1920
+    @width=1920
     Window.height=1080
-    @@height=1080
+    @height=1080
     Window.windowed=false
     Window.caption="Blooming the background"
     Window.fps=200
-    @@i=940
+    @carsoru = Sprite.new(200, 200, Image.new(10,15))
+    @startfont=Sprite.new(950,540,Image.new(100,25))
+    @endfont=Sprite.new(980,640,Image.new(100,25))
+
+    @i_start=940    #startButtonの変数
+    @a_start=0
+
+    @i_end=970      #endButtonの変数
+    @a_end=0
+
+
   end
+
   def startButton()
-    Window.draw_font(950,540,"Bloom",Font.new(30))
 
-     if (@@i<1400)
-      Window.draw_line(940,570,@@i,570,[255, 255, 255, 255])
-      @@i+=1
+    Window.draw_font_ex(1300,571,"Bloom",Font.new(30),{angle:180,alpha:@a_start})
+    Window.draw_font_ex(950,540,"Bloom",Font.new(30))
+   @@startfont.draw
+    if (@startfont===@carsoru)
+     if (@i_start<1400)
+      Window.draw_line(940,570,@i_start,570,[255, 255, 255, 255])
+       @i_start+=3
+       @a_start+=0.5
+     else
+       Window.draw_line(940,570,@i_start,570,[255, 255, 255, 255])
+     end
     else
-       Window.draw_line(940,570,@@i,570,[255, 255, 255, 255])
-     end
-     end
-     end
+     @i_start=940
+     @a_start=0
+   end
+  end
 
-t=Title.new()
-f=Myfps.new()
-Window.loop do
-  t.startButton()
-  f.rightUpFps()
+  def endButton()
+
+    Window.draw_font_ex(1300,671,"Wither",Font.new(30),{angle:180,alpha:@a_end})
+    Window.draw_font_ex(980,640,"Wither",Font.new(30))
+    @endfont.draw
+    if (@endfont===@carsoru)
+     if (@i_end<1400)
+      Window.draw_line(970,670,@i_end,670,[255, 255, 255, 255])
+      @i_end+=3
+      @a_end+=0.5
+    else
+       Window.draw_line(970,670,@i_end,670,[255, 255, 255, 255])
+     end
+   else
+     @i_end=970
+     @a_end=0
+   end
+  end
+
+  def carsoru()
+
+    @carsoru.x = Input.mouse_pos_x
+    @carsoru.y = Input.mouse_pos_y
+    @carsoru.draw
+  end
+
+  def close()
+    if Input.key_push?(K_Q)
+      Window.close
+    end
+  end
 end
