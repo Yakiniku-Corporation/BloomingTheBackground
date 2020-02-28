21086ba9679f4535048baf533d5d897e5e5e64b7
Mamoliena <54253130+Mamoliena@users.noreply.github.com>
2020-02-28 18:55:56 +0900

    Add files via upload

--- option.rb
+++ option.rb
@@ -1,10 +1,63 @@
-require'dxruby'
 class Option
   def initialize()
-    @@black       = Image.new(1920,1080,[0,0,0])
-    @@option_back = Image.load("image/material/option_back.png")
+    @black             = Image.new(1920,1080,[0,0,0])
+    @option_background = Image.load("image/material/option_back.png")
+
+    @ESCkey_flag       = false
+    @black_fade_flag   = false
+    @fade_flag         = false
+    @fade              = 0
+    @rot               = -90
+    @move              = 990
+    @OB_Y              = -1005
   end
-  def option()
-    Window.draw(0,0,@white)
+
+  def input_ESC
+    if Input.key_push?(K_ESCAPE)
+      flagman(1)
+      flagman(2) if()
+      @fade,@@rot=0,-90
+      p(Input.mouse_x,Input.mouse_y)
+    end
+    option()
+  end
+
+  def flagman(type)
+    case(type)
+
+    when(1)
+      if(@ESCkey_flag == false)
+        @ESCkey_flag = true
+      elsif(@ESCkey_flag == true)
+        @ESCkey_flag = false
+      end
+
+    when(2)
+      if(@fade_flag == false)
+        @fade_flag = true
+      elsif(@fade_flag == true)
+        @fade_flag = false
+      end
+
+    when(3)
+      if(@rot_flag == false)
+        @rot_flag = true
+      elsif(@rot_flag == true)
+        @rot_flag = false
+      end
+    end
+  end
+
+  def option
+    if(@ESCkey_flag==true)
+      if(@fade_flag==false)
+        Window.draw_alpha(0,0,@black,@fade+=5)
+        flagman(2) if(@fade>=200)
+      elsif(@fade_flag==true)
+        Window.draw_alpha(0,0,@black,@fade)
+      end
+
+      Window.draw_rot(0,@OB_Y, @option_background,@rot)
+    end
   end
 end
--- window.rb
+++ window.rb
@@ -1,17 +1,19 @@
 require'dxruby'
 require'./option.rb'
-#coding:shift_jis
-#Window.caption     = "Blooming the background"
-#Window.width       = @window_width  = 1920
-#Window.height      = @window_height = 1080
-#Window.full_screen = true
+#shift_jis
+Window.caption     = "Blooming the background"
+Window.width       = @window_width  = 1920
+Window.height      = @window_height = 1080
+Window.full_screen = true
 
-@option_draw = Option.new()
-@white       = Image.new(1920,1080,[255,255,255])
+$option_draw = Option.new()
+$white       = Image.new(1920,1080,[255,255,255])
 
 Window.loop do
-  Window.draw(0,0,@white)
-  if Input.key_down?(K_ESCAPE)
-    @option_draw.option()
-  end
+
+  Window.draw(0,0,$white)
+
+  $option_draw.input_ESC
+
+  break if Input.key_down?(K_Q)
 end
