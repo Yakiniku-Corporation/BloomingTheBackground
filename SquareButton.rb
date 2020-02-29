8d816b2cd631e28d8b45be5899421bf7cec73481
nisini <61419352+nisini@users.noreply.github.com>
2020-02-29 21:15:47 +0900

    正方形のボタンです
    文字は1つしか表示できません。最終的にはButton.classにまとめるつもりです。

---
+++ SquareButton.rb
@@ -0,0 +1,31 @@
+
+require 'dxruby'
+require './Carsoru'
+
+class SquareButton
+attr_accessor:x,:y
+
+  def initialize
+    @c=Carsoru.new()
+  end
+
+  def draw_square_Button(x,y,moji)   #(x座標,y座標,文字1つ)
+    if((moji.length) != 1)
+       p "error:draw_square_Button:文字数は1つに限られます"
+    else
+     Window.draw_scale(self.x=x,self.y=y,Image.load('button.png'),0.2,0.5)
+     Window.draw_font_ex(self.x+103,self.y+35,moji,Font.new(30))
+     @c.draw_carsoru()
+     @SquareButton=Sprite.new(x+92,y+33,Image.new(38,35))
+     @SquareButton.draw
+     if(@c.carsoru===@SquareButton)
+      Window.draw_scale(self.x=x,self.y=y,Image.load('button2.png'),0.2,0.5)
+      Window.draw_font_ex(self.x+103,self.y+35,moji,Font.new(30),color:C_BLACK)
+    else
+      Window.draw_scale(self.x=x,self.y=y,Image.load('button.png'),0.2,0.5)
+      Window.draw_font_ex(self.x+103,self.y+35,moji,Font.new(30))
+     end
+   end
+
+  end
+end
